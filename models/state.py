4f870a002431d44784d4255f83d25cb9466146b2
mohrahman <mohrahman558@gmail.com>
2023-12-09 02:20:33 +0000

    state module

---
+++ models/state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+state module
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    State class
+    Parent:
+        BaseModel
+    """
+
+    name = ""
