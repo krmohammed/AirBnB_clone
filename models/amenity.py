749125dfe2bf818936124dc922951d097004dbe3
mohrahman <mohrahman558@gmail.com>
2023-12-09 02:18:35 +0000

    amenity module

---
+++ models/amenity.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+amenity module
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Amenity class
+    Parent:
+        BaseModel
+    """
+
+    name = ""
