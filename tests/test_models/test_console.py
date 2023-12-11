494db4dcbc073bb7887cd60bcaceefc8f9615480
mohrahman <mohrahman558@gmail.com>
2023-12-11 01:13:40 +0000

    added: unittest file for console module

---
+++ tests/test_models/test_console.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Test cases for console
+"""
+import unittest
+from models import storage
+
+
+class TestConsole(unittest.TestCase):
+    """
+    console test cases
+    """
+
+    def test_consolt(self):
+        """
+        this is a test case
+        """
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
